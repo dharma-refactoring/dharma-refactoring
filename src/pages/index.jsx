a0085dd80532ca7548f69e52fc09f2f1a7c57dc1
yoshimasa36g <ashihana785@gmail.com>
2019-10-01 23:37:10 +0900

    tweak

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,17 +2,16 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+import EyeCatch from "../components/eye-catch"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>🚧このサイトは現在鋭意作成中です🚧</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+      <EyeCatch />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
